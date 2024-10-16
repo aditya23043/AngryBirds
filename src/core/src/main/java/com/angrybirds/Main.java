cf034dd9a90fa9d7c38cbe5568a91160e3a2d914
Aditya Gautam <adityascottish27@gmail.com>
2024-10-16 16:24:47 +0530

    libgdx stuff

--- src/core/src/main/java/com/angrybirds/Main.java
+++ src/core/src/main/java/com/angrybirds/Main.java
@@ -6,29 +6,34 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Color;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private BitmapFont font;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        font = new BitmapFont();
+        font.setColor(Color.RED);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        font.draw(batch, "Java sucks", 200, 200);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        font.dispose();
     }
 }
