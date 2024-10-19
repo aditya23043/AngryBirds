8e0ae33fa8743662ca1863abe60d8ba1cc54dffe
Aditya Gautam <adityascottish27@gmail.com>
2024-10-19 21:31:23 +0530

    minor changed

---
+++ devlog.md
@@ -0,0 +1,7 @@
+# ANGRY BIRDS GAME DEVELOPMENT LOG
+
+> Note: I started this dev log after figuring out gradle, gdx-liftoff, and how to add skins in libgdx\
+> Note2: All my source code for the 2nd submission is in the `src/` directory
+
+## Skin
+- Added the shade ui skin from https://github.com/czyzby/gdx-skins to my `src/assets/skins/shade`
--- src/core/src/main/java/com/angrybirds/Main.java
+++ src/core/src/main/java/com/angrybirds/Main.java
@@ -2,54 +2,99 @@ package com.angrybirds;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Main extends ApplicationAdapter {
 
-    private Skin skin;
-    private Stage stage;
-    private Table table;
+	private Skin skin;
+	private Texture bg_texture;
+	private Image bg_image;
+	private Stage stage;
+	private Table table;
 
-    @Override
-    public void create() {
-        skin = new Skin(Gdx.files.internal("skins/shade/uiskin.json"));
+	@Override
+	public void create() {
 
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
+		// BG IMAGE
+		bg_texture = new Texture(Gdx.files.internal("img/vecteezy_background-for-presentation-green-grass-with-flower-under_17308322-1.jpg"));
+		bg_image = new Image(bg_texture);
+		bg_image.setFillParent(true);
 
-        table = new Table();
-        table.setFillParent(true);
-        stage.addActor(table);
+		// LOADING SKIN
+		skin = new Skin(Gdx.files.internal("skins/shade/uiskin.json"));
 
-		Label label = new Label("Something", skin, "title");
-        table.add(label);
+		// STAGE
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
 
-		Touchpad touchpad = new Touchpad(10, skin, "default");
-        table.add(touchpad);
-    }
+		// TABLE (LAYOUT)
+		table = new Table();
+		table.setFillParent(true);
+		stage.addActor(bg_image);
+		stage.addActor(table);
 
-    @Override
-    public void render() {
-        Gdx.gl.glClearColor(.9f, .9f, .9f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act();
-        stage.draw();
-    }
+		// MAIN TITLE
+		Label title_label = new Label("Angry Birds", skin, "title");
+		title_label.setFontScale(2);
 
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
+		// BUTTONS
+		TextButton playButton = new TextButton("Play", skin);
+		TextButton newLevelButton = new TextButton("New Level", skin);
+		TextButton loadLevelButton = new TextButton("Load Level", skin);
+		TextButton exitButton = new TextButton("Exit", skin);
 
-    @Override
-    public void dispose() {
-        skin.dispose();
-        stage.dispose();
-    }
+		// PLACEMENT (not the btech one lol)
+		table.top().pad(50);
+		table.add(title_label).center().padBottom(50);
+		table.row();
+        table.add(playButton).width(200).padBottom(20);  // Set button width and padding
+        table.row();
+        table.add(newLevelButton).width(200).padBottom(20);
+        table.row();
+        table.add(loadLevelButton).width(200).padBottom(20);
+        table.row();
+        table.add(exitButton).width(200).padBottom(20);
+
+		// QUIT EVENT
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();  // Quit the application when exit is clicked
+            }
+        });
+
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(.9f, .9f, .9f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act();
+		stage.draw();
+		if (Gdx.input.isKeyJustPressed(Input.Keys.Q)) {
+			Gdx.app.exit();
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void dispose() {
+		skin.dispose();
+		stage.dispose();
+	}
 }
