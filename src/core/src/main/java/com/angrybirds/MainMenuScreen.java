59efb0dc3d932d053dbb7f64544f5b41d9828a59
Aditya Gautam <adityascottish27@gmail.com>
2024-10-20 16:44:26 +0530

    refactored code for Game class

---
+++ src/core/src/main/java/com/angrybirds/MainMenuScreen.java
@@ -0,0 +1,106 @@
+package com.angrybirds;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class MainMenuScreen extends ScreenAdapter {
+
+    private Stage stage;
+    private Viewport viewport;
+    private Skin skin;
+    private Table table;
+    private BitmapFont font;
+
+    @Override
+    public void show() {
+        // custom font
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/FiraSansCondensed-Bold.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 64;  // Set the font size
+        parameter.color = Color.WHITE;  // Font color
+        font = generator.generateFont(parameter);  // Generate BitmapFont
+        generator.dispose();  // Dispose of the generator when done
+
+        // viewport
+        viewport = new ExtendViewport(960, 540);
+        stage = new Stage(viewport);
+
+        // skin
+        skin = new Skin(Gdx.files.internal("skins/shade/uiskin.json"));
+        skin.add("default-font", font);
+
+        // table
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        // main title
+        Label main_title = new Label("Angry Birds", skin, "title");
+        table.padTop(20);
+        table.add(main_title).center().padBottom(50);
+        table.row();
+
+        // buttons
+        button_add("Play").addListener(new ClickListener(){
+            
+        });
+        button_add("Select Level");
+        button_add("Help");
+        button_add("Exit").addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        Gdx.input.setInputProcessor(stage);
+
+    }
+
+    private TextButton button_add(String button_text) {
+        TextButton button = new TextButton(button_text, skin);
+        button.padTop(10);
+        button.padBottom(10);
+        button.padLeft(20);
+        button.padRight(20);
+        table.add(button).fillX().padBottom(10);
+        table.row();
+        return button;
+    }
+    
+    private BitmapFont font_set(String font_name, int font_size, Color color){
+
+        return font;
+    }
+
+    @Override
+    public void render(float delta) {
+        
+        Gdx.gl.glClearColor(.1f, .1f, .1f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+}
+
