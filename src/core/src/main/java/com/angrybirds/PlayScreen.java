0cf53b48680cc89ad9337b5a08c74b84e7be5173
Aditya Gautam <adityascottish27@gmail.com>
2024-10-20 21:39:36 +0530

    added pigs

--- src/core/src/main/java/com/angrybirds/PlayScreen.java
+++ src/core/src/main/java/com/angrybirds/PlayScreen.java
@@ -69,8 +69,20 @@ public class PlayScreen extends ScreenAdapter {
 
         // credits
         Label bg_credit = new Label("bg credits: https://in.pinterest.com/pin/616571005271619075/", skin, "default");
-        bg_credit.setPosition(viewport.getWorldWidth()/2+200, 10);
+        bg_credit.setPosition(viewport.getWorldWidth()/2+100, 10);
         stage.addActor(bg_credit);
+
+        Label stone_credit = new Label("stone texture credits: Image by kues1 on Freepik", skin, "default");
+        stone_credit.setPosition(viewport.getWorldWidth()/2+100, 20);
+        stage.addActor(stone_credit);
+
+        Label wood_credit = new Label("wood texture credits: Image by kbza on Freepik", skin, "default");
+        wood_credit.setPosition(viewport.getWorldWidth()/2+100, 30);
+        stage.addActor(wood_credit);
+
+        Label catapult_credit = new Label("catapult texture credits: https://www.klipartz.com/en/sticker-png-xulig", skin, "default");
+        catapult_credit.setPosition(viewport.getWorldWidth()/2+100, 40);
+        stage.addActor(catapult_credit);
 
         // catapult
         Texture catapult_tex = new Texture("img/catapult.png");
@@ -91,6 +103,64 @@ public class PlayScreen extends ScreenAdapter {
         red_bird_2.setScale(1.2f);
         red_bird_2.setPosition(20, 170);
         stage.addActor(red_bird_2);
+
+        // stone
+        Texture stone_tex = new Texture("img/stone.png");
+        Image stone = new Image(stone_tex);
+        stone.setSize(50, 50);
+        stone.setPosition(700, 174);
+        stage.addActor(stone);
+        Texture stone_2_tex = new Texture("img/stone.png");
+        Image stone_2 = new Image(stone_2_tex);
+        stone_2.setSize(50, 50);
+        stone_2.setPosition(700, 224);
+        stage.addActor(stone_2);
+
+        // wood
+        Texture wood_tex = new Texture("img/wood.png");
+        Image wood = new Image(wood_tex);
+        wood.setSize(50, 50);
+        wood.setPosition(600, 174);
+        stage.addActor(wood);
+        Texture wood_2_tex = new Texture("img/wood.png");
+        Image wood_2 = new Image(wood_2_tex);
+        wood_2.setSize(50, 50);
+        wood_2.setPosition(600, 224);
+        stage.addActor(wood_2);
+
+        // glass
+        Texture glass_tex = new Texture("img/glass.png");
+        Image glass = new Image(glass_tex);
+        glass.setSize(50, 50);
+        glass.setPosition(500, 174);
+        stage.addActor(glass);
+        Texture glass_2_tex = new Texture("img/glass.png");
+        Image glass_2 = new Image(glass_2_tex);
+        glass_2.setSize(50, 50);
+        glass_2.setPosition(500, 224);
+        stage.addActor(glass_2);
+
+        // pigs
+        Texture pig_1_tex = new Texture("img/pig1.png");
+        Image pig_1 = new Image(pig_1_tex);
+        pig_1.setScale(1f);
+        pig_1.setSize(32, 32);
+        pig_1.setPosition(500+9, 274);
+        stage.addActor(pig_1);
+
+        Texture pig_2_tex = new Texture("img/pig1.png");
+        Image pig_2 = new Image(pig_2_tex);
+        pig_2.setScale(1.5f);
+        pig_2.setSize(32, 32);
+        pig_2.setPosition(600, 274);
+        stage.addActor(pig_2);
+
+        Texture pig_3_tex = new Texture("img/pig1.png");
+        Image pig_3 = new Image(pig_3_tex);
+        pig_3.setScale(2f);
+        pig_3.setSize(32, 32);
+        pig_3.setPosition(700-9, 274);
+        stage.addActor(pig_3);
 
         Gdx.input.setInputProcessor(stage);
 
